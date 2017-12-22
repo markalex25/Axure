e28783a38f7777d61c172bb50fa2e65763bd9b88
markalex25 <markalex25@163.com>
2017-12-22 15:35:11 +0800

    售后/维修原型初版

---
+++ 有市App/HTML/files/首页/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(bf,_(bg,bh,bi,bj),w,bk),S,_(),bl,_(),bm,g)])),bn,_(),bo,_(bp,_(bq,br)));}; 
+var b="url",c="首页.html",d="generationDate",e=new Date(1513644437362.03),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="9991fde84fba46189449b7ad3baea970",q="type",r="Axure:Page",s="name",t="首页",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="d1d59db4129a403d9f5615e78fd4f66e",Y="label",Z="friendlyType",ba="矩形",bb="vectorShape",bc="styleType",bd="visible",be=true,bf="size",bg="width",bh=375,bi="height",bj=812,bk="47641f9a00ac465095d6b672bbdffef6",bl="imageOverrides",bm="generateCompound",bn="masters",bo="objectPaths",bp="d1d59db4129a403d9f5615e78fd4f66e",bq="scriptId",br="u10";
+return _creator();
+})());
